2dd948da0349ba975214b9f65ec673ca2b0da72e
Mark Hatle <mark.hatle@amd.com>
2026-07-03 10:34:18 -0600

    test-statx.c: Move from pragma to updating compiler cflags
    There are some concerns that pragmas may be more fragile then cflags.
    
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- test/test-statx.c
+++ test/test-statx.c
@@ -10,9 +10,6 @@
 #include <fcntl.h>
 #include <errno.h>
 
-// Passing a null pointer is the test scenario
-#pragma GCC diagnostic ignored "-Wnonnull"
-
 int statx_test() {
     if (statx(0, NULL, 0, 0, NULL) != -1) {
         return 1;
